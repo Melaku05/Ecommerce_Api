90a819e206efb3bdf4c64778bd8f59f996210c75
melaku05 <melakushiferaw05@gmail.com>
2022-09-26 12:19:40 +0300

    Create custom email backend

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,15 @@
+from django.core.mail import  BadHeaderError
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'John'},
+        )
+        message.send(['jhon@melaku.com'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
