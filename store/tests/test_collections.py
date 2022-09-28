04fb10553000da22239aac97d00df278cb111694
melaku05 <melakushiferaw05@gmail.com>
2022-09-28 12:40:06 +0300

    Create automated test for collection end point

---
+++ store/tests/test_collections.py
@@ -0,0 +1,11 @@
+from rest_framework import status
+from rest_framework.test import APIClient
+import pytest
+
+
+@pytest.mark.django_db
+class TestCreateCollection:
+  def test_if_user_is_anonymous_returns_401(self):
+    client = APIClient()
+    response = client.post('/collections/', {'title':'a'}) 
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED 
