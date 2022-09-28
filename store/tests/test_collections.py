489e2529afed0f910626b5da3ec5ffa074a7dd31
melaku05 <melakushiferaw05@gmail.com>
2022-09-28 14:10:24 +0300

    AUthenticationg the user

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -9,3 +9,9 @@ class TestCreateCollection:
     client = APIClient()
     response = client.post('/collections/', {'title':'a'}) 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED 
+   # Authenticating the user (The client is authenticated, but the current user is not an admin)
+  def test_if_user_is_not_admin_returns_403(self):
+    client = APIClient()
+    client.force_authenticate({})
+    response = client.post('/collections/', {'title':'a'}) 
+    assert response.status_code == status.HTTP_403_FORBIDDEN
