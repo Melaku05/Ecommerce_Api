0a89f9367545b5294d52407eca337219946d0f9b
melaku05 <melakushiferaw05@gmail.com>
2022-09-28 15:58:27 +0300

    Implement Fixtures to create clear test

---
+++ store/tests/conftest.py
@@ -0,0 +1,12 @@
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+import pytest
+
+@pytest.fixture
+def authenticate(api_client):
+    def do_authenticate(is_staff=False):
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
+    return do_authenticate
+@pytest.fixture
+def api_client():
+    return APIClient()
