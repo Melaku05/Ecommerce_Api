a2bb0bf4387b6eb38b6ae3845112fbf2f7627d33
melaku05 <melakushiferaw05@gmail.com>
2022-09-29 16:15:37 +0300

    Implement cache on class based views

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,14 @@
+from django.core.cache import cache
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
 import requests
 
-def say_hello(request):
-    requests.get('https://httpbin.org/delay/2')
-    return render(request, 'hello.html', {'name': 'Mosh'})
 
+class SayHelloView(APIView):
+    @method_decorator(cache_page(5*60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': data})
