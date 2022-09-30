cf4f51e25f402f8f0c2b5cbd9db4f0a315484062
melaku05 <melakushiferaw05@gmail.com>
2022-09-30 13:41:01 +0300

    Change dev settings file exist location

--- storefront/urls.py
+++ storefront/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('playground/', include('playground.urls')),
-    path('store', include('store.urls')),
+    path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include(debug_toolbar.urls)),
